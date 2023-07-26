922da2cfdf21dcb592b267ea9300f4b99d7d8c9f
Max Lennon <mrl7ja@virginia.edu>
2023-07-26 17:17:26 -0400

    Added API imports

---
+++ discuss_anything.py
@@ -0,0 +1,11 @@
+import pywikibot
+import elevenlabs
+from elevenlabs import clone, generate, play
+
+elevenlabs.set_api_key("key")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
