739f5df8dc54a0a6147718f2ed0b5293c1471b6b
max-lennon <mrlennon@cs.unc.edu>
2023-07-29 01:43:17 -0400

    Added VLC file saving & playing functionality

--- discuss_anything.py
+++ discuss_anything.py
@@ -1,6 +1,9 @@
+import os
+import time
 import pywikibot
 from elevenlabs import generate, stream,set_api_key, voices, play, save
-# import vlc
+import vlc
+import soundfile as sf
 from AccessTokens import ELEVENLABS_API_KEY
 
 # Set up ElevenLabs credentials
@@ -16,14 +19,28 @@ print(voices_)
 def get_topic():
     pass
 
-def read_message(message_text: str):
-    audio_stream = generate(
+def read_message(message_text: str, use_streaming: bool = False):
+    audio_output = generate(
         text=message_text,
         voice=ROBOT_VOICE,
         model="eleven_monolingual_v1",
-        stream=True
+        stream=use_streaming,
     )
-    stream(audio_stream)
+
+    if use_streaming:
+        stream(audio_output)
+    else:
+        discussion_tts = os.path.join(os.path.abspath(os.curdir), f"latest_discussion_topic.mp3")
+        save(audio_output, discussion_tts)
+        p = vlc.MediaPlayer(discussion_tts)
+        p.play()
+        wav_file = sf.SoundFile(discussion_tts)
+        length_of_wav = wav_file.frames / wav_file.samplerate
+        wav_file.close()
+        time.sleep(length_of_wav + 1)
+        p.stop()
+
+    
 
 def begin_discussion():
     message = "BEGIN DISCUSSION!"
